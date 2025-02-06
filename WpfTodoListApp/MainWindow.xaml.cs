a048be8842f41f6d200b25fb8a6a7f1b3ee6cc7f
Aleksei Parokonnyi <aleksei.parokonnyi@gmail.com>
2025-02-06 19:48:42 +0200

    init

---
+++ WpfTodoListApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WpfTodoListApp
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public ObservableCollection<string> TodoTasks { get; set; } = new();
+
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      DataContext = this;
+    }
+
+    private void AddTaskClick(object sender, RoutedEventArgs e)
+    {
+      var taskInput = TodoTaskInput.Text;
+
+      if (string.IsNullOrEmpty(taskInput))
+      {
+        MessageBox.Show("Task cannot be empty");
+        return;
+      }
+
+      TodoTasks.Add(taskInput);
+    }
+
+    private void OpenTaskCreationWindow(object sender, RoutedEventArgs e)
+    {
+      TaskCreationWindow taskWindow = new TaskCreationWindow();
+
+      if (taskWindow.ShowDialog() == true)
+      {
+        TodoTasks.Add(taskWindow.TaskResult);
+      }
+    }
+  }
+}
---
+++ WpfTodoListApp/TaskCreationWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace WpfTodoListApp;
+
+/// <summary>
+/// Interaction logic for TaskCreationWindow.xaml
+/// </summary>
+public partial class TaskCreationWindow : Window
+{
+  public string TaskResult { get; private set; } = string.Empty;
+
+  public TaskCreationWindow()
+  {
+    InitializeComponent();
+  }
+
+  private void AddTaskClick(object sender, RoutedEventArgs e)
+  {
+    if (string.IsNullOrWhiteSpace(TaskInput.Text))
+    {
+      MessageBox.Show("Task cannot be empty");
+      return;
+    }
+
+    TaskResult = TaskInput.Text;
+    DialogResult = true;
+  }
+}
